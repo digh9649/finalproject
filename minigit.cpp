781f597ff580e0252950097c605ed3cc8821db8c
digh9649 <digh9649@colorado.edu>
2021-04-17 19:58:51 -0600

    adding minigit.cpp

---
+++ minigit.cpp
@@ -0,0 +1,29 @@
+#include "minigit.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+ 
+using namespace std; 
+ 
+bool searchHelper(singlyNode*head)
+{
+ 
+}
+ 
+void minigit::remove(string filename, singlyNode *head)
+{
+    bool exist =  searchHelper(head); 
+    if(exist)
+    {
+        if(head->fileName == filename)
+        {
+            
+        }
+    }
+}
+ 
+int main()
+{
+    
+}
