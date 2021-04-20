5e2d13e78b3ae169d16e2f8409a6e7fb29a8607e
digh9649 <digh9649@colorado.edu>
2021-04-20 15:21:26 -0600

    adding driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,64 @@
+#include "minigit.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <filesystem>
+using namespace std; 
+
+
+void displayMenu()
+{
+    cout << "===== Main Menu =====" << endl; 
+    cout << "1. Initialize new repository" <<endl; 
+    cout << "2. Add a file" << endl; 
+    cout << "3. Remove a file" << endl; 
+    cout << "4. Commit" << endl; 
+    cout << "5. Checkout" <<endl; 
+    cout << "6. Quit" << endl; 
+}
+
+int main()
+{
+    int input; 
+    string filenameAdd = "";
+    string filenameRemove = "";  
+    int commitNumber;
+    displayMenu(); 
+    cin >> input; 
+
+    while(1)
+    {
+        if(input ==1)
+        {
+            cout << "new repository initialized" << endl; 
+        }
+        else if (input ==2)
+        {
+            cout << "Enter file name" << endl; 
+            cin >> filenameAdd; 
+        }
+        else if(input ==3)
+        {
+            cout << "Enter file name" << endl; 
+            cin >> filenameRemove; 
+        }
+        else if(input ==4)
+        {
+            cout << "Files have been commited" << endl; 
+        }
+        else if(input ==5)
+        {
+            cout << "Enter a commit number" << endl; 
+            cin >> commitNumber; 
+        }
+        else if(input==6)
+        {
+            cout << "Goodbye!" << endl; 
+            break; 
+        }
+        displayMenu(); 
+        cin>>input; 
+    }
+    return 0; 
+}
